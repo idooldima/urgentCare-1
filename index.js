8a71182bf1fc7732b6e32efe1457108a752aebc5
Dmitrijlagutkin <dmitrijlagutkin@gmail.com>
2023-01-20 22:52:59 +0200

    handle capcha error

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ function sendMail() {
 
   const CERVICE_ID = "service_6g4b9sf";
   const TEMPLETE_ID = "template_fnlcymh";
-
-  emailjs
-    .send(CERVICE_ID, TEMPLETE_ID, data)
-    .then((res) => {
-      document.getElementById("name").value = "";
-      console.log(res);
-    })
-    .then(() => grecaptcha.reset())
-    .catch((err) => console.log(err));
+  if (!!capcha) {
+    emailjs
+      .send(CERVICE_ID, TEMPLETE_ID, data)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        console.log(res);
+      })
+      .then(() => grecaptcha.reset())
+      .catch((err) => console.log(err));
+  } else {
+    console.log("Please capcha");
+  }
 }
